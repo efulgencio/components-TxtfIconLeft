61324a6b73876774eb8d68cc4c82e6bac9adea3b
eduardo <efulgenciocomendeiro@gmail.com>
2020-01-12 11:16:42 +0100

    Finaliza la edición para ocultar el teclado.

--- UITextFieldAbout/ViewController.swift
+++ UITextFieldAbout/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
         
         let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
               calcBtn.backgroundColor = #colorLiteral(red: 1, green: 0.5917804241, blue: 0.0205632858, alpha: 1)
-              calcBtn.setTitle("Calculate", for: .normal)
+              calcBtn.setTitle("Formatear", for: .normal)
               calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
               calcBtn.addTarget(self, action: #selector(ViewController.formatear), for: .touchUpInside)
               
@@ -29,6 +29,7 @@ class ViewController: UIViewController {
     @objc func formatear() {
         if let wageTxt = txtficonLeft.text {
             // formatear el campo
+            self.view.endEditing(true)
         }
     }
 
